974afeb315f3731a2152c264e68a83e8af4d33da
njcarlo <njcarlo@gmail.com>
2018-03-14 10:37:13 +0800

    commit

---
+++ SampleforMetroFramwork/Register.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SampleforMetroFramwork
+{
+    public partial class Register : MetroFramework.Forms.MetroForm
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTile2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+            StudentInfo Studinfo = new StudentInfo();
+            Studinfo.Show();
+        }
+    }
+}
---
+++ SampleforMetroFramwork/RegisterMenu.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SampleforMetroFramwork
+{
+    public partial class RegisterMenu : MetroFramework.Forms.MetroForm
+    {
+
+        public RegisterMenu()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void StudentInfo_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void metroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroDateTime1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroCheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            Payment Paymenu = new Payment();
+            Paymenu.Show();
+            this.Hide();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            Menuform RegF = new Menuform();
+            RegF.Show();
+            this.Hide();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroDateTime1_ValueChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel14_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel19_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox6_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox9_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox16_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox20_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox8_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroCheckBox2_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel16_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel6_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel11_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel12_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroCheckBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroCheckBox2_CheckedChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroCheckBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox7_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox10_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
